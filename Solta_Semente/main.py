68a609c83f115a7446fbf9a6059fa376f21f66b2
Ant.C <acarlosms1983@gmail.com>
2025-08-20 13:36:44 -0300

    Solta_semente pronto

--- Solta_Semente/main.py
+++ Solta_Semente/main.py
@@ -2,24 +2,19 @@ import os
 import time
 
 def limpar_tela():
-    """Limpa a tela do console"""
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def imprimir_sala(mapa_sala):
-    """Imprime o mapa da sala no console"""
+def imprimir_campo(mapa_campo):
     limpar_tela()
     
-    for linha in mapa_sala:
+    for linha in mapa_campo:
         print(''.join(linha))
     
     time.sleep(0.2)
 
-def limpar_sala(mapa_sala, posicao_inicial):
-    """
-    Faz o robô limpar toda a sala em padrão simples da esquerda para a direita
-    """
+def campo_marcado(mapa_campo, posicao_inicial):
     # Cria uma cópia do mapa para trabalhar
-    mapa_trabalho = [linha[:] for linha in mapa_sala]
+    mapa_trabalho = [linha[:] for linha in mapa_campo]
     
     # Obtém as dimensões do mapa
     linhas = len(mapa_trabalho)
@@ -28,12 +23,15 @@ def limpar_sala(mapa_sala, posicao_inicial):
     # Marca a posição inicial
     y_inicial, x_inicial = posicao_inicial
     mapa_trabalho[y_inicial][x_inicial] = 'O'
-    imprimir_sala(mapa_trabalho)
+    imprimir_campo(mapa_trabalho)
     
     # Para cada linha do mapa (de cima para baixo)
     for linha in range(1, linhas-1):
-        # Decide a direção da varredura (da esquerda para a direita)
-        inicio, fim, passo = 1, colunas-1, 1
+        # Decide a direção da varredura baseada na paridade da linha
+        if linha % 2 == 1:  # Linhas ímpares: esquerda para direita
+            inicio, fim, passo = 1, colunas-1, 1
+        else:  # Linhas pares: direita para esquerda
+            inicio, fim, passo = colunas-2, 0, -1
         
         # Para cada coluna na direção atual
         for coluna in range(inicio, fim, passo):
@@ -43,20 +41,20 @@ def limpar_sala(mapa_sala, posicao_inicial):
             
             # Move o robô para a nova posição
             mapa_trabalho[linha][coluna] = 'O'
-            imprimir_sala(mapa_trabalho)
+            imprimir_campo(mapa_trabalho)
             
-            # Marca a posição como limpa
+            # Marca a posição com semente
             mapa_trabalho[linha][coluna] = '*'
     
     # Retorna para a posição inicial
     mapa_trabalho[y_inicial][x_inicial] = 'O'
-    imprimir_sala(mapa_trabalho)
+    imprimir_campo(mapa_trabalho)
     
-    print("Limpeza concluída!")
+    print("Sementes soltas!")
 
-def main():
+def Soltar():
     # Mapa da sala 10x10 com obstáculos
-    mapa_sala = [
+    mapa_campo = [
         ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
         ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
         ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
@@ -72,18 +70,17 @@ def main():
     # Posição inicial do robô (linha, coluna)
     posicao_inicial = (1, 1)
     
-    print("Simulador de Robô Aspirador")
+    print("Simulador de Robô Solta_Semente")
     print("Legenda:")
-    print("  # - Parede/obstáculo")
+    print("  # - Limites")
     print("  O - Robô")
-    print("  * - Área limpa")
-    print("  ' ' - Área suja")
+    print("  * - Área plantada")
+    print("  ' ' - Área vazia")
     input("\nPressione Enter para iniciar...")
     
-    # Inicia a limpeza
-    limpar_sala(mapa_sala, posicao_inicial)
+    # Inicia a plantação
+    campo_marcado(mapa_campo, posicao_inicial)
     
     input("\nPressione Enter para sair...")
 
-if __name__ == "__main__":
-    main()
+Soltar()
